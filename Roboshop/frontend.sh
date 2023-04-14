6a36741a85fbe6d151546395e226293349088567
prabhu <prabhucharan3172@gmail.com>
2023-04-14 16:35:43 +0530

    More Clean scripted

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/bash
+source common.sh
 #Roboshop -Frontend Script
 ##################################
-
+echo Installing Nginx
 yum install nginx -y
- systemctl enable nginx
- systemctl start nginx
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
- cd /usr/share/nginx/html
- rm -rf *
- unzip /tmp/frontend.zip
- mv frontend-main/static/* .
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
- systemctl restart nginx
+StatusCheck
+echo Enabling Nginx
+systemctl enable nginx
+StatusCheck
+echo Starting Nginx
+systemctl start nginx
+StatusCheck
+echo Downloading Frontend
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend.log
+StatusCheck
+cd /usr/share/nginx/html
+rm -rf *
+echo Unziping Frontend Files
+unzip /tmp/frontend.zip &>>/tmp/frontend.log && mv frontend-main/static/* . && mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+StatusCheck
+echo Restarting Nginx
+systemctl restart nginx
+StatusCheck
