eac333d7a1eb889a00f1581a2dbdbc3890ebb61a
prabhu <prabhucharan3172@gmail.com>
2023-05-08 19:49:29 +0530

    files added

--- Learning-shell/print.sh
+++ Learning-shell/print.sh
@@ -9,3 +9,34 @@ echo todays date is $date1
 a=$((2+19/4-12))
 echo "mathamatic calculation of a is $a"
 
+## Q1 if Condition statement
+
+touch sample
+if [ $? -eq 0 ]; then
+    echo Hello
+else
+    echo Not working
+fi
+## Q2 sum of 100 numbers
+
+echo Enter A number
+read num
+i=1
+sum=0
+while [ $i -le $num ]; do
+  sum=$((i + sum))
+  i=$((i+1))
+done
+
+echo "the sum of $num is = $sum"
+
+##Q3 Joining two variables
+
+echo Enter a number
+Num_1=Hello
+Num_2=World
+Num_1+=$Num_2
+echo $Num_1
+
+##Q4
+
